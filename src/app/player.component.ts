35d67e848588882d417e2eaea667e1ef79105d40
Tero Parviainen <tero@teropa.info>
2016-08-26 07:21:17 -0400

    animation PoC

--- src/app/player.component.ts
+++ src/app/player.component.ts
@@ -1,24 +1,54 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/core';
 import { PlayerState } from './models';
 
 @Component({
   selector: '[in-c-player]',
   template: `
-    <svg:circle [attr.cx]="getX()" cy="100" r="50"></svg:circle>
+    <svg:circle [attr.cx]="getX()"
+                cy="100"
+                r="50"
+                [@nowPlaying]="isPlaying() ? 'yes' : 'no'">
+    </svg:circle>
   `,
   styles: [`
     circle {
       fill: white;
     }
   `],
+  animations: [
+    trigger('nowPlaying', [
+      state('yes', style({opacity: 1})),
+      state('no',  style({opacity: 0.5})),
+      transition('* => *', animate('1000ms'))
+    ])
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlayerComponent {
   @Input() playerState: PlayerState;
   @Input() screenWidth: number;
 
+  playing = true;
+
+  constructor() {
+    setTimeout(() => this.playing = false, 1000);
+  }
   getX() {
     const relativeX = (this.playerState.player.position + 1) / 2;
     return relativeX * this.screenWidth;
   }
+
+  isPlaying() {
+    return this.playing;
+  }
+
 }

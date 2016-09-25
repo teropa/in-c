3b91b9f3f9e13cc6295ee86a14bdaaa6bbf81362
Tero Parviainen <tero@teropa.info>
2016-09-25 12:10:42 +0300

    use @ngrx/effects for pulse

--- src/app/core/pulse.service.ts
+++ src/app/core/pulse.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, Inject, NgZone } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Effect, mergeEffects } from '@ngrx/effects';
+import { Store, Action } from '@ngrx/store';
 import { AppState } from './app-state.model';
 import { PULSE } from './actions';
 import { TimeService } from './time.service';
@@ -9,19 +12,23 @@ const MetronomeWorker = require('worker!./metronome.worker');
 @Injectable()
 export class PulseService {
   private startTime: number;
-  private metronome: Worker;
+  private metronome = new MetronomeWorker();
+  private messages$ = Observable.fromEvent(this.metronome, 'message');
+  private storeSubscription: Subscription;
   private pulseCount = 1;
+
+  @Effect() pulse$ = this.messages$
+    .concatMap(() => this.makePulses());
 
   constructor(@Inject('bpm') private bpm: number,
               private time: TimeService,
               private store: Store<AppState>,
               private zone: NgZone) {
+    this.storeSubscription = mergeEffects(this).subscribe(store);
   }
 
   onInit() {
     this.startTime = this.time.now();
-    this.metronome = new MetronomeWorker();
-    this.metronome.onmessage = (evt => this.zone.run(() => this.makePulses()));
     this.metronome.postMessage({
       command: 'start',
       interval: this.getBeatInterval() * 1000
@@ -30,13 +37,16 @@ export class PulseService {
 
   onDestroy() {
     this.metronome.terminate();
+    this.storeSubscription.unsubscribe();
   }
 
   private makePulses() {
+    const pulses: Action[] = [];
     while (this.getNextPulseTime() - this.time.now() < this.getBeatInterval() * 2) {
       this.pulseCount++;
-      this.store.dispatch({type: PULSE, payload: {time: this.getNextPulseTime(), bpm: this.bpm}});
+      pulses.push({type: PULSE, payload: {time: this.getNextPulseTime(), bpm: this.bpm}});
     }
+    return pulses;
   }
 
   private getBeatInterval() {
